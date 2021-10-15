559e5f2aa8ef70bc88c52f2f4c442199cf60d5e7
medhichemlemsi <hichem.lemsi@gmail.com>
2021-10-15 14:40:40 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+
+import imageInSrc from "./imageInSrc.png";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div style={{ border: "solid 1px black", maxWidth: "100vw" }}>
+        <h1 className="title red">Your name here</h1>
+        <br />
+        <img src={imageInSrc} style={{ width: 200 }} alt="imageInSrc" />
+        <br />
+        <img
+          src="./imageInPublic.jpg"
+          style={{ width: 200 }}
+          alt="imageInPublic"
+        />
+      </div>
+
+      <video width="320" height="240" controls>
+        <source src="./myVideo.mp4" type="video/mp4" />
+      </video>
+    </div>
+  );
+}
+
+export default App;
